f77e11fa21cb64165d2cf5ce3d13ef3cd78a1c77
Lars <Bruger@DESKTOP-DUN3DKK>
2025-12-30 18:49:56 +0100

    Adds Models and folders

---
+++ DFDExam.App/Models/SqlUser.cs
@@ -0,0 +1,8 @@
+namespace DFDExam.App.Models;
+
+public class SqlUser
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = null!;
+    public string Email { get; set; } = null!;
+}
